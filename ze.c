7508d39896aa63fd20a316784b5a68791922b79e
zachwick <zach@zachwick.com>
2018-05-15 16:15:08 -0400

    Implements a basic backspace
    Signed-off-by: zachwick <zach@zachwick.com>

--- ze.c
+++ ze.c
@@ -310,6 +310,18 @@ editorRowInsertChar(erow *row, int at, int c)
   E.dirty++;
 }
 
+void
+editorRowDelChar(erow *row, int at)
+{
+  if (at < 0 || at >= row->size) {
+    return;
+  }
+  memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
 /*** editor operations ***/
 
 void
@@ -320,6 +332,20 @@ editorInsertChar(int c)
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void
+editorDelChar()
+{
+  if (E.cy == E.numrows) {
+    return;
+  }
+
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  }
 }
 
 /*** file i/o ***/
@@ -640,7 +666,10 @@ editorProcessKeypress()
   case BACKSPACE:
   case CTRL_KEY('h'):
     /*case DEL_KEY:*/
-    /* TODO */
+    /*if (c == DEL_KEY) {
+        editorMoveCursor(ARROW_RIGHT);
+      }*/
+    editorDelChar();
     break;
   case PAGE_UP:
   case PAGE_DOWN:
