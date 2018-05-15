dd8cb3f32ecfcd5538e02186742a66608d5cf1e4
zachwick <zach@zachwick.com>
2018-05-14 21:47:12 -0400

    Adds HOME and END support
    Signed-off-by: zachwick <zach@zachwick.com>

--- ze.c
+++ ze.c
@@ -37,7 +37,9 @@ enum editorKey {
   ARROW_UP = CTRL_KEY('p'),
   ARROW_DOWN = CTRL_KEY('n'),
   PAGE_UP = CTRL_KEY('k'),
-  PAGE_DOWN = CTRL_KEY('v')
+  PAGE_DOWN = CTRL_KEY('v'),
+  HOME_KEY = CTRL_KEY('a'),
+  END_KEY = CTRL_KEY('e')
 };
 
 /*** data ***/
@@ -120,8 +122,12 @@ editorReadKey()
 	}
 	if (seq[2] == '~') {
 	  switch (seq[1]) {
+	  case '1': return HOME_KEY;
+	  case '4': return END_KEY;
 	  case '5': return PAGE_UP;
 	  case '6': return PAGE_DOWN;
+	  case '7': return HOME_KEY;
+	  case '8': return END_KEY;
 	  }
 	}
       } else {
@@ -130,7 +136,14 @@ editorReadKey()
 	case 'B': return ARROW_DOWN;
 	case 'C': return ARROW_RIGHT;
 	case 'D': return ARROW_LEFT;
+	case 'H': return HOME_KEY;
+	case 'F': return END_KEY;
 	}
+      }
+    } else if (seq[0] == '0') {
+      switch (seq[1]) {
+      case 'H': return HOME_KEY;
+      case 'F': return END_KEY;
       }
     }
     return '\x1b';
@@ -304,6 +317,12 @@ editorProcessKeypress()
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
+    break;
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+  case END_KEY:
+    E.cx = E.screencols - 1;
     break;
   case PAGE_UP:
   case PAGE_DOWN:
