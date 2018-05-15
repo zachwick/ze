52bcd9f63979b993a4d0fcce43a36bd5ada63b6a
zachwick <zach@zachwick.com>
2018-05-14 20:12:28 -0400

    More efficient line drawing
    Signed-off-by: zachwick <zach@zachwick.com>

--- ze.c
+++ ze.c
@@ -175,6 +175,7 @@ editorDrawRows(struct abuf *ab)
   for (y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
 
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -186,12 +187,13 @@ editorRefreshScreen()
 {
   struct abuf ab = ABUF_INIT;
 
-  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
