0e0a3bbcd323bc5a0e4947cc03134988e81086ba
zachwick <zach@zachwick.com>
2018-05-14 22:08:56 -0400

    more WIP printing file contents
    Signed-off-by: zachwick <zach@zachwick.com>

--- ze.c
+++ ze.c
@@ -17,6 +17,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  **/
+
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -208,15 +213,28 @@ getWindowSize(int *rows, int *cols)
 /*** file i/o ***/
 
 void
-editorOpen() {
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
+editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) {
+    die("fopen");
+  }
 
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
+      linelen--;
+    }
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
 }
 
 /** append buffer ***/
@@ -389,11 +407,13 @@ initEditor()
 }
 
 int
-main()
+main(int argc, char *argv[])
 {
   enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
   while (1) {
     editorRefreshScreen();
