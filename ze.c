29631ddd2732f6bd963fb8112ca1a76a5e4dedb4
zachwick <zach@zachwick.com>
2018-05-16 09:11:15 -0400

    Implements highlighting single line comments
    Signed-off-by: zachwick <zach@zachwick.com>

--- ze.c
+++ ze.c
@@ -57,6 +57,7 @@ enum editorKey {
 
 enum editorHighlight {
   HL_NORMAL = 0,
+  HL_COMMENT,
   HL_STRING,
   HL_NUMBER,
   HL_MATCH
@@ -69,6 +70,7 @@ enum editorHighlight {
 struct editorSyntax {
   char *filetype;
   char **filematch;
+  char *singleline_comment_start;
   int flags;
 };
 
@@ -107,6 +109,7 @@ struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
+    "//",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
@@ -279,6 +282,9 @@ editorUpdateSyntax(erow *row)
     return;
   }
 
+  char *scs = E.syntax->singleline_comment_start;
+  int scs_len = scs ? strlen(scs) : 0;
+
   int prev_sep = 1;
   int in_string = 0;
 
@@ -286,6 +292,13 @@ editorUpdateSyntax(erow *row)
   while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if (scs_len && !in_string) {
+      if (!strncmp(&row->render[i], scs, scs_len)) {
+	memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+	break;
+      }
+    }
 
     if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
       if (in_string) {
@@ -329,6 +342,8 @@ int
 editorSyntaxToColor(int hl)
 {
   switch (hl) {
+  case HL_COMMENT:
+    return 36;
   case HL_STRING:
     return 35;
   case HL_NUMBER:
