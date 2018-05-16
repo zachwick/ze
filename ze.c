e0e11ff783315a0c14d64f50fcbfeb7574828442
zachwick <zach@zachwick.com>
2018-05-15 23:22:31 -0400

    Implements color coding of search results
    Signed-off-by: zachwick <zach@zachwick.com>

--- ze.c
+++ ze.c
@@ -57,7 +57,8 @@ enum editorKey {
 
 enum editorHighlight {
   HL_NORMAL = 0,
-  HL_NUMBER
+  HL_NUMBER,
+  HL_MATCH
 };
 
 /*** data ***/
@@ -258,6 +259,8 @@ editorSyntaxToColor(int hl)
   switch (hl) {
   case HL_NUMBER:
     return 31;
+  case HL_MATCH:
+    return 34;
   default:
     return 37;
   }
@@ -549,6 +552,15 @@ editorFindCallback(char *query, int key)
   static int last_match = -1;
   static int direction = 1;
 
+  static int saved_hl_line;
+  static char *saved_hl = NULL;
+
+  if (saved_hl) {
+    memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
+
   if (key == '\r' || key == '\x1b') {
     last_match = -1;
     direction = 1;
@@ -581,6 +593,11 @@ editorFindCallback(char *query, int key)
       E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      saved_hl_line = current;
+      saved_hl = malloc(row->rsize);
+      memcpy(saved_hl, row->hl, row->rsize);
+      memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
   }
