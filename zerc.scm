f9f684116f4988def829d5ffa199065e680f1566
zach wick <zach@zachwick.com>
2020-02-03 11:42:57 -0500

    Adds example zerc.scm file
    Signed-off-by: zach wick <zach@zachwick.com>

---
+++ zerc.scm
@@ -0,0 +1,24 @@
+(define notes_template '"/Users/zwick/.ze/notes")
+(define readme_template '"/Users/zwick/.ze/readme")
+
+(define ze_config
+  (lambda ()
+    (display "Loading ze config") (newline)))
+
+(define (preSaveHook contents)
+  (number->string (string-length contents)))
+
+(define (postSaveHook contents)
+  (string-append (string-append "Wrote " (number->string (string-length contents)) " bytes to file")))
+
+(define (preDirOpenHook dirname)
+  (string-append "Opening directory " dirname))
+
+(define (postDirOpenHook numFiles)
+  (string-append (number->string numFiles) " files"))
+
+(define (preFileOpenHook filename)
+  (string-append "Opening file " filename))
+
+(define (postFileOpenHook contents)
+  (string-append (string-append "Read " (number->string (string-length contents)) " bytes from file")))
